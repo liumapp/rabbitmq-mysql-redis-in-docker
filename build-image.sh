ee43b5c09e113dbde6b3781049fb8327b3b88ab3
liumapp <liumapp.com@gmail.com>
2018-05-18 14:26:53 +0800

    add build mysql , redis and rabbitmq image in docker

---
+++ build-image.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo '============================================================='
+echo '$                                                           $'
+echo '$                      liumapp                              $'
+echo '$                                                           $'
+echo '$                                                           $'
+echo '$  email:    liumapp.com@gmail.com                          $'
+echo '$  homePage: http://www.liumapp.com                         $'
+echo '$  Github:   https://github.com/liumapp                     $'
+echo '$                                                           $'
+echo '============================================================='
+echo '.'
+
+docker pull mysql:5.5.60
+
+docker pull rabbitmq:3-management
+
+docker pull redis:3.2.11
+
